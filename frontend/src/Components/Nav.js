58d534946be1064dc9e1be8c34569b31128e9470
manjot503 <manjotsingh5503@gmail.com>
2024-04-25 11:23:35 +0530

    sa

--- frontend/src/Components/Nav.js
+++ frontend/src/Components/Nav.js
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
-
+import { PiUserCircleFill } from "react-icons/pi";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
@@ -11,10 +11,10 @@ function  BasicExample() {
     <>
        <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
        <Container>
-         <Navbar.Brand href="#home">BlOg</Navbar.Brand>
+         <Navbar.Brand id='icons' href="/signup"><PiUserCircleFill /></Navbar.Brand>
          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-         <Navbar.Collapse id="basic-navbar-nav ">
-           <Nav className=' d-flex'>
+         <Navbar.Collapse id="basic-navbar-nav justify-contact "className=' d-flex-end'>
+           <Nav >
             
            <Nav><Link className="nav-link fs-5 mx-3 active" to="/">Home</Link></Nav>
              {/* <Nav><Link className="btn bg-white text-success mx-1 "  to="/login">login</Link></Nav>
